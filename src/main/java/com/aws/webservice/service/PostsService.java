bb4a7f152e3ac84a4fee63fb5f9d08f0abb0b4bc
kimbs <sckbs1314@naver.com>
2021-12-22 23:58:45 +0900

    3장법사

---
+++ src/main/java/com/aws/webservice/service/PostsService.java
@@ -0,0 +1,40 @@
+package com.aws.webservice.service;
+
+import com.aws.webservice.domain.posts.Posts;
+import com.aws.webservice.domain.posts.PostsRepository;
+import com.aws.webservice.web.dto.PostsResponseDto;
+import com.aws.webservice.web.dto.PostsSaveRequestDto;
+import com.aws.webservice.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. ID = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. ID = " + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+}
--- src/test/java/com/aws/webservice/web/WebApplicationTests.java
+++ src/test/java/com/aws/webservice/web/WebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.aws.web;
+package com.aws.webservice.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
