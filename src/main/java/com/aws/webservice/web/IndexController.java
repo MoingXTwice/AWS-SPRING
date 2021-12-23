da59c4efdc9cc3ef92daa2b4d653833f3af1e259
kimbs <sckbs1314@naver.com>
2021-12-23 20:22:24 +0900

    사5정

--- src/main/java/com/aws/webservice/web/IndexController.java
+++ src/main/java/com/aws/webservice/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.aws.webservice.web;
 
+import com.aws.webservice.config.auth.LoginUser;
+import com.aws.webservice.config.auth.dto.SessionUser;
 import com.aws.webservice.service.PostsService;
 import com.aws.webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
+
         model.addAttribute("posts", postsService.findAllDesc());
 
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
