f48f0da18a0e478036d5931dcb2cf65c6001356d
kimbs <sckbs1314@naver.com>
2021-12-23 22:22:59 +0900

    gradlew 수정 test

--- src/main/java/com/aws/webservice/domain/posts/Posts.java
+++ src/main/java/com/aws/webservice/domain/posts/Posts.java
@@ -7,14 +7,13 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-import static javax.persistence.GenerationType.*;
-
 @Getter
 @NoArgsConstructor
 @Entity
 public class Posts extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 500, nullable = false)
@@ -25,15 +24,15 @@ public class Posts extends BaseTimeEntity {
 
     private String author;
 
-    public void update(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
     @Builder
     public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
